dd5b3be65362c10728ff03fb4d24d8ac093249ca
Alex Yalovyi <yalovyialex@gmail.com>
2024-08-09 19:52:57 +0200

    rewrite bot

--- index.js
+++ index.js
@@ -6,20 +6,41 @@ const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzcRUaKySaeYh
 const LAMBDA_WEBHOOK_URL = 'https://zaii5o5q6f.execute-api.us-east-1.amazonaws.com/webhook'
 
 exports.handler = async (event) => {
-    const botMessage = "Hello, this is the bot from telegram";
+    const requestBody = JSON.parse(event.body);
 
-    const sendText = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage?chat_id=${CHATID}&parse_mode=HTML&text=${encodeURIComponent(botMessage)}`;
+    const requestMsg = requestBody.message;
+
+    const chatId = requestMsg.chat.id;
+
+    let command = requestMsg.text.trim();
+
+    if (command.startsWith('/')) {
+        command = command.substring(1);
+    }
+
+    let message;
+
+    // Визначаємо відповідь залежно від команди
+    if (command === 'start') {
+        message = "Welcome to my bot! How can I help you today?"; // Відповідь на команду /start
+    } else if (command === 'help') {
+        message = "Here are the available commands: /start, /help"; // Відповідь на команду /help
+    } else {
+        message = "I'm sorry, I didn't understand that command. Please try again."; // Відповідь на невідому команду
+    }
+
+    // Формуємо URL для запиту до Telegram API
+    const sendText = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage?chat_id=${chatId}&parse_mode=HTML&text=${encodeURIComponent(message)}`;
+
 
     const response = await new Promise((resolve, reject) => {
         https.get(sendText, (res) => {
             let data = '';
 
-            // A chunk of data has been received.
             res.on('data', (chunk) => {
                 data += chunk;
             });
 
-            // The whole response has been received.
             res.on('end', () => {
                 resolve(data);
             });
@@ -28,6 +49,7 @@ exports.handler = async (event) => {
         });
     });
 
+    console.log(sendText);
     console.log(response);
 
     return {
