de4f72f6dc265643742cf8c41532f3e1e8f4da5e
Alex Yalovyi <yalovyialex@gmail.com>
2024-08-10 19:58:17 +0200

    update chatbot_without_gateway

--- index.mjs
+++ index.mjs
@@ -6,40 +6,43 @@ const sendMsgToBot = async (msg, chatId) =>{
 }
 
 export const handler = async (event) => {
-    const requestBody = JSON.parse(event.body);
+    // const requestBody = JSON.parse(event.body);
 
-    const requestMsg = requestBody.message;
+    // const requestMsg = requestBody.message;
 
-    const chatId = requestMsg.chat.id;
+    // const chatId = requestMsg.chat.id;
 
-    let command = requestMsg.text.trim();
+    // let command = requestMsg.text.trim();
 
-    if (command.startsWith('/')) {
-        command = command.substring(1);
-    }
+    // if (command.startsWith('/')) {
+    //     command = command.substring(1);
+    // }
 
-    let message;
+    // let message;
 
-    if (command === 'start') {
-        message = "Welcome to my bot! How can I help you today?";
-    } else if (command === 'help') {
-        message = "Here are the available commands: /start, /help, /send_stats";
-    } else if (command === 'send_stats') {
-        try{
-            await sendMsgToBot("Try to send analytics to your email", chatId)
-            await axios.post(process.env.GOOGLE_SCRIPT_URL);
-            message = "Check your email";
-        } catch(e) {
-            console.error('error', e);
-            await sendMsgToBot("Something went wrong", chatId)
-        }
-    } else {
-        message = "I'm sorry, I didn't understand that command. Please try again.";
-    }
+    // if (command === 'start') {
+    //     message = "Welcome to my bot! How can I help you today?";
+    // } else if (command === 'help') {
+    //     message = "Here are the available commands: /start, /help, /send_stats";
+    // } else if (command === 'send_stats') {
+    //     try{
+    //         await sendMsgToBot("Try to send analytics to your email", chatId)
+    //         await axios.post(process.env.GOOGLE_SCRIPT_URL);
+    //         message = "Check your email";
+    //     } catch(e) {
+    //         console.error('error', e);
+    //         await sendMsgToBot("Something went wrong", chatId)
+    //     }
+    // } else {
+    //     message = "I'm sorry, I didn't understand that command. Please try again.";
+    // }
 
-    const response = await sendMsgToBot(message, chatId)
+    // const response = await sendMsgToBot(message, chatId)
 
-    console.log('success response', response);
+    // console.log('success response', response);
+
+    await sendMsgToBot("I will send it every 12 hours", process.env.CHAT_ID)
+
 
     return {
         statusCode: 200,
