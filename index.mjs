a87dc4d7339b10bde6ceedd1c307ce4224035e68
Alex Yalovyi <yalovyialex@gmail.com>
2024-08-12 20:21:26 +0200

    add checking for 3.97 rate

--- index.mjs
+++ index.mjs
@@ -43,8 +43,14 @@ export const handler = async (event) => {
 
     
     const res = await axios.get('https://rest.coinapi.io/v1/exchangerate/USD/PLN?apikey=044008B4-27A0-437E-8F26-842F42A4CFE9')
+
+    const rate = Number(res.data.rate)
     console.log('res', res)
-    await sendMsgToBot(`Coin api PLN rate = ${res.data.rate}`, process.env.CHAT_ID)
+    console.log('rate', rate)
+
+    if (rate > 3.97) {
+        await sendMsgToBot(`Coin api PLN rate = ${rate}!!! It's time to sell!`, process.env.CHAT_ID)
+    }
 
 
     return {
