6f32624ca0a1787294804e1c3622cd8d5f595369
Alex Yalovyi <yalovyialex@gmail.com>
2024-08-16 19:19:08 +0200

    chore: add prettier and use toFixed

--- index.mjs
+++ index.mjs
@@ -1,29 +1,39 @@
-import axios from 'axios';
+import axios from "axios";
 
-const sendMsgToBot = async (msg, chatId) =>{
-    const sendText = `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage?chat_id=${chatId}&parse_mode=HTML&text=${encodeURIComponent(msg)}`;
-    return await axios.get(sendText);
-}
-
-export const handler = async (event) => {
-    const res = await axios.get('https://rest.coinapi.io/v1/exchangerate/USD/PLN?apikey=044008B4-27A0-437E-8F26-842F42A4CFE9')
-
-    const rate = Number(res.data.rate)
-    console.log('res', res)
-    console.log('rate', rate)
-
-    if (rate > 3.97) {
-        await sendMsgToBot(`Coin api PLN rate = ${rate}!!! It's time to sell USD to PLN!!!`, process.env.CHAT_ID)
-    }
-
-    if (rate < 3.91) {
-        await sendMsgToBot(`Coin api PLN rate = ${rate}!!! It's time to buy USDT!!!`, process.env.CHAT_ID)
-    }
-
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!')
-    };
+const sendMsgToBot = async (msg, chatId) => {
+  const sendText = `https://api.telegram.org/bot${
+    process.env.TELEGRAM_TOKEN
+  }/sendMessage?chat_id=${chatId}&parse_mode=HTML&text=${encodeURIComponent(
+    msg
+  )}`;
+  return await axios.get(sendText);
 };
 
+export const handler = async () => {
+  const res = await axios.get(
+    "https://rest.coinapi.io/v1/exchangerate/USD/PLN?apikey=044008B4-27A0-437E-8F26-842F42A4CFE9"
+  );
+
+  const rate = Number(res.data.rate).toFixed(2);
+  console.log("res", res);
+  console.log("rate", rate);
+
+  if (rate > 3.97) {
+    await sendMsgToBot(
+      `Coin api PLN rate = ${rate}!!! It's time to sell USD to PLN!!!`,
+      process.env.CHAT_ID
+    );
+  }
+
+  if (rate < 3.91) {
+    await sendMsgToBot(
+      `Coin api PLN rate = ${rate}!!! It's time to buy USDT!!!`,
+      process.env.CHAT_ID
+    );
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+};
