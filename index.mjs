0919e52d340a050cee1a1a7aed29db0ef0cee623
Alex Yalovyi <yalovyialex@gmail.com>
2024-09-07 19:30:14 +0200

    add check-air function

--- index.mjs
+++ index.mjs
@@ -11,26 +11,22 @@ const sendMsgToBot = async (msg, chatId) => {
 
 export const handler = async () => {
   const res = await axios.get(
-    "https://rest.coinapi.io/v1/exchangerate/USD/PLN?apikey=044008B4-27A0-437E-8F26-842F42A4CFE9"
+    "https://api.waqi.info/feed/geo:50.0438719;19.9907768/?token=4c2de55087433a8c208a462a3da340e45dae14b7"
   );
 
-  const rate = Number(res.data.rate).toFixed(2);
-  console.log("res", res);
-  console.log("rate", rate);
+  console.log("status", res.status);
+  console.log("aqi", res.data.aqi);
+  console.log("idx", res.data.idx);
 
-  if (rate > 3.97) {
-    await sendMsgToBot(
-      `Coin api PLN rate = ${rate}!!! It's time to sell USD to PLN!!!`,
-      process.env.CHAT_ID
-    );
-  }
+  await sendMsgToBot(
+    `Air quality index by aqicn ${res.data.aqi}`,
+    process.env.CHAT_ID
+  );
 
-  if (rate < 3.91) {
-    await sendMsgToBot(
-      `Coin api PLN rate = ${rate}!!! It's time to buy USDT!!!`,
-      process.env.CHAT_ID
-    );
-  }
+  await sendMsgToBot(
+    `Rest data: pm25 ${res.data.iaqi.pm25}, pm10 ${res.data.iaqi.pm10}, forecast daily pm25 ${res.data.forecast.daily.mp25[0].avg}`,
+    process.env.CHAT_ID
+  );
 
   return {
     statusCode: 200,
